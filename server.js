92fddefaef482202a1d81dee58307e94fec4ba52
JudithRohrer <59763566+JudithRohrer@users.noreply.github.com>
2020-04-25 12:40:49 +0200

    Added fs module - readFile

--- server.js
+++ server.js
@@ -13,6 +13,16 @@ http.createServer((request, response) => {
     filePath = 'index.html';
   }
 
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      throw err;
+    }
+
+    response.writeHead(200, {'Content-Type': 'text/html' });
+    response.write(data);
+    response.end();
+  });
+
   fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', (err) => {
     if (err) {
       console.log(err);
