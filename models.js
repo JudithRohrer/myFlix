90bae7ef65ef6e63f83d19b97283db06344e3698
JudithRohrer <59763566+JudithRohrer@users.noreply.github.com>
2020-05-24 19:02:46 +0200

    Update models.js

--- models.js
+++ models.js
@@ -30,7 +30,7 @@ userSchema.statics.hashPassword = (password) => {
 };
 
 userSchema.methods.validatePassword = (password) => {
-  return bcrypt.compareSync(password, this.password);
+  return bcrypt.compareSync(password, this.Password);
 };
 
 let Movie = mongoose.model('Movie', movieSchema, 'movies');
