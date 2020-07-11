f6f230069ad1a7e9705880902046a3a2a73525bb
JudithRohrer <59763566+JudithRohrer@users.noreply.github.com>
2020-07-11 15:50:26 +0200

    updated origins

--- index.js
+++ index.js
@@ -21,17 +21,18 @@ app.get("/client/*", (req, res) => {
 //give certain origins access
 let allowedOrigins = ['https://myflix-123-db.herokuapp.com/client/', 'http://localhost:1234', '*'];
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn't found on the list of allowed allowedOrigins
-      let message = 'The CORS policy for this application does not allow access from origin' + origin;
-      return callback(new Error(message ), false);
-    }
-    return callback(null, true);
-  }
-}));
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if(!origin) return callback(null, true);
+//     if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn't found on the list of allowed allowedOrigins
+//       let message = 'The CORS policy for this application does not allow access from origin' + origin;
+//       return callback(new Error(message ), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
 
+ app.use(cors())
 
 // Integrating authentication
 let auth = require('./auth')(app);
